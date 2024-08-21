f6b6688c63ce5bbba6830e432e45bed30712b124
garlingz <166535626+garlingz@users.noreply.github.com>
2024-08-21 12:22:31 -0500

    Bug Fixes
    simplifying code for the heatmap, and formatting done when drawing the heatmap, rather than to the dataframe.
    Changed BMI to correctly apply to values HIGHER than 25 (was previously >= 25).

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,56 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+df = pd.read_csv('medical_examination.csv', index_col= 'id')
 
-# 2
-df['overweight'] = None
+def calculate_BMI(row):
+    #   Convert to Meters
+    height_meters = row['height'] * 0.01
+    bmi = round((row['weight'] / (height_meters ** 2)), 2)
+    return bmi
 
-# 3
-
-
-# 4
+#   Formatting a few categories
+df['BMI'] = df.apply(calculate_BMI, axis= 1)
+df['overweight'] = df['BMI'] > 25
+df['overweight'] = df['overweight'].astype(int)
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
+df = df.reset_index()
+# Drawing the first plot
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    df_cat = pd.melt(df,
+                        id_vars= 'cardio', 
+                        value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'],
+                        var_name= 'variable',
+                        value_name= 'value'
+                        )
+    df_grouped = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name= 'count')
 
-
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    fig = sns.catplot(data= df_grouped, x= 'variable', y= 'count', col= 'cardio', hue= 'value', kind= 'bar')
     fig.savefig('catplot.png')
     return fig
 
-
-# 10
+# Drawing the second plot
 def draw_heat_map():
-    # 11
-    df_heat = None
+    lower_height = df['height'].quantile(0.025)
+    upper_height = df['height'].quantile(0.975)
+    lower_weight = df['weight'].quantile(0.025)
+    upper_weight = df['weight'].quantile(0.975)
 
-    # 12
-    corr = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= lower_height) &
+        (df['height'] <= upper_height) &
+        (df['weight'] >= lower_weight) &
+        (df['weight'] <= upper_weight)
+    ]
+    df_heat = df_heat.drop('BMI', axis= 1)
 
-    # 13
-    mask = None
+    corr = df_heat.corr().round(1)
+    mask = np.triu(np.ones_like(corr, dtype= bool))
 
+    fig, ax = plt.subplots(figsize= (10, 8))
+    sns.heatmap(data= corr, mask= mask, fmt= ".1f", annot= True, ax= ax)
 
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
     fig.savefig('heatmap.png')
     return fig
